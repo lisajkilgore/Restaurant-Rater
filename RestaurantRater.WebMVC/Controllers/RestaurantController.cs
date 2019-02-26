5309209fa07879503c93b3e0250a2ca0a7ef548c
Jason McIntosh <lisajkilgore@gmail.com>
2019-02-26 14:22:43 -0500

    added index

--- RestaurantRater.WebMVC/Controllers/RestaurantController.cs
+++ RestaurantRater.WebMVC/Controllers/RestaurantController.cs
@@ -89,6 +89,20 @@ namespace RestaurantRater.WebMVC.Controllers
             return View(restaurant);
         }
 
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+
+            if (restaurant == null)
+                return HttpNotFound();
+
+            return View(restaurant);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
